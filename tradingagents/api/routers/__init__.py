80a106933d4c23133bff3c1490e174dbafd17750
Ahmed Rabie <ahmed.rabie1234@gmail.com>
2026-02-17 22:02:20 +0300

    feat(04-03): wire historical router into api
    - import historical router into versioned api
    - include /historical routes under api_router

--- tradingagents/api/routers/__init__.py
+++ tradingagents/api/routers/__init__.py
@@ -3,6 +3,7 @@
 from fastapi import APIRouter
 
 from tradingagents.api.routers.auth import router as auth_router
+from tradingagents.api.routers.historical import router as historical_router
 from tradingagents.api.routers.markets import router as markets_router
 from tradingagents.api.routers.snapshots import router as snapshots_router
 from tradingagents.api.routers.symbols import router as symbols_router
@@ -12,5 +13,6 @@ api_router.include_router(auth_router)
 api_router.include_router(markets_router)
 api_router.include_router(symbols_router)
 api_router.include_router(snapshots_router)
+api_router.include_router(historical_router)
 
 __all__ = ["api_router"]
